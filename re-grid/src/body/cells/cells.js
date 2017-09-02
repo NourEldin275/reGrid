04307dd43dca3bf20027c066b3c6c702c8d8d783
Nour Eldin Ali <noureldin.ali.m@gmail.com>
2017-09-02 21:00:12 +0200

    solved issue where pagination didn't show the new data for editable fields. Next is to submit data after editing.

--- re-grid/src/body/cells/cells.js
+++ re-grid/src/body/cells/cells.js
@@ -4,6 +4,16 @@
 import React, { Component } from 'react';
 
 class EditableCell extends Component{
+
+    constructor(props){
+        super(props);
+        this.changeHandler = this.changeHandler.bind(this);
+    }
+
+    changeHandler(event){
+        this.props.changeHandler(event, this.props.cellKey);
+    }
+
     getInput(){
         const settings = this.props.editing;
         const textType = 'text';
@@ -14,7 +24,7 @@ class EditableCell extends Component{
         let input = <td>{this.props.defaultValue}</td>;
         switch (settings.type){
             case textType:
-                input = <input type="text" value={this.props.defaultValue.toString()}/>;
+                input = <input type="text" value={this.props.defaultValue.toString()} onChange={this.changeHandler}/>;
                 break;
             case numberType:
                 if(settings.options){
@@ -24,26 +34,28 @@ class EditableCell extends Component{
                         min={settings.options.min}
                         max={settings.options.max}
                         step="any"
+                        onChange={this.changeHandler}
                     />;
                 }else{
                     input = <input
                         type="number"
                         value={this.props.defaultValue.toString()}
                         step="any"
+                        onChange={this.changeHandler}
                     />;
                 }
                 break;
             case checkboxType:
-                input = <input type="checkbox" defaultChecked={this.props.defaultValue}/>;
+                input = <input type="checkbox" defaultChecked={this.props.defaultValue} onChange={this.changeHandler}/>;
                 break;
             case selectType:
                 let options = settings.options.options.map((option) =>
-                    <option key={settings.options.options.indexOf(option)} value={option.value}>{option.label}</option>
+                    <option key={settings.options.options.indexOf(option)} value={option.value} onChange={this.changeHandler}>{option.label}</option>
                 );
                 input = <select
                     name=""
                     id=""
-                    value={this.props.defaultValue}>{options}</select>;
+                    value={this.props.defaultValue} onChange={this.changeHandler}>{options}</select>;
                 break;
             default:
                 break;
@@ -65,6 +77,8 @@ function Cell(props) {
             <EditableCell
                 defaultValue={props.value}
                 editing={props.editing}
+                changeHandler={props.changeHandler}
+                cellKey={props.cellKey}
             />
         );
     }
--- re-grid/src/body/rows/row.js
+++ re-grid/src/body/rows/row.js
@@ -6,14 +6,44 @@ import { Cell } from '../cells/cells';
 
 class Row extends Component{
 
+    constructor(props){
+        super(props);
+        this.cellChangeHandler = this.cellChangeHandler.bind(this);
+        this.state = {
+            rowData: this.props.rowData
+        }
+    }
+
+    componentWillReceiveProps(newProps){
+        this.setState({rowData: newProps.rowData});
+    }
+
+
+    cellChangeHandler(event, cellId){
+        let rowData = this.state.rowData;
+        let updated = rowData.map((cell) => {
+            if(cell.id === cellId){
+                rowData[rowData.indexOf(cell)].value = event.target.value;
+                return true;
+            }
+            return false;
+        });
+
+        if(updated){
+            this.setState({rowData});
+        }
+    }
+
     render(){
-        let row = this.props.rowData;
+        let row = this.state.rowData;
         let rowCells = row.map((cell) => {
             return (
                 <Cell
                     key={cell.id.toString()}
                     value={cell.value}
                     editing={cell.editing}
+                    changeHandler = {this.cellChangeHandler}
+                    cellKey={cell.id.toString()}
                 />
             );
         });
